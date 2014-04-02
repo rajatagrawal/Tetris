33c7895b336c7da0fd3f9549b0e5ba44d0d0c2b9
Rajat Agrawal <agrawal.rajat.89@gmail.com>
2014-04-02 05:45:55 -0400

    Update main file to use game strategy module

--- lib/tetris/main.rb
+++ lib/tetris/main.rb
@@ -1,10 +1,29 @@
 require 'gosu'
+require_relative 'game_strategy/main'
 
 module Tetris
   class GameWindow < Gosu::Window
+
     def initialize
       super(1280,1080,false)
       self.caption=('Tetris')
+      config = { width: 6,
+                 height: 20,
+                 unit_side: 40 }
+      @game_strategy = GameStrategy::Main.new(self, config)
+    end
+
+    def update
+      sleep(0.1)
+      @game_strategy.run_game
+    end
+
+    def draw
+      @game_strategy.shapes.each do |shape|
+        shape.draw
+      end
     end
   end
 end
+game = Tetris::GameWindow.new
+game.show
