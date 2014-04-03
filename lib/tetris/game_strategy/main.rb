04268d23df93671771bcd23fe2f24cd62f38ec1f
Rajat Agrawal <agrawal.rajat.89@gmail.com>
2014-04-03 05:23:42 -0400

    Use TetrisMap and Squeeze module in game

--- lib/tetris/game_strategy/main.rb
+++ lib/tetris/game_strategy/main.rb
@@ -1,6 +1,8 @@
 require_relative 'window'
 require_relative 'movement'
 require_relative 'game_control'
+require_relative 'squeeze'
+require_relative 'tetris_map'
 
 module Tetris
   module GameStrategy
@@ -8,6 +10,8 @@ module Tetris
       include Window
       include Movement
       include GameControl
+      include Squeeze
+      include TetrisMap
     end
   end
 end
