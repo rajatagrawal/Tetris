8d911a7c768bcdbed59c65a82319fad2291bc947
Rajat Agrawal <agrawal.rajat.89@gmail.com>
2014-04-05 10:54:50 -0400

    Add game_strategy file to require all files in a separate file

--- lib/tetris/game_strategy/main.rb
+++ lib/tetris/game_strategy/main.rb
@@ -1,16 +1,4 @@
-require_relative '../shape/i_shape'
-require_relative '../shape/j_shape'
-require_relative '../shape/l_shape'
-require_relative '../shape/o_shape'
-require_relative '../shape/s_shape'
-require_relative '../shape/t_shape'
-require_relative '../shape/z_shape'
-require_relative 'tetris_map'
-require_relative 'movement'
-require_relative 'squeeze'
-require_relative 'score'
-require_relative 'game_control'
-
+require_relative 'game_strategy'
 module Tetris
   module GameStrategy
     class Main
