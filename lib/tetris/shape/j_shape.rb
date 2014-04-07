c2b2c0194ed9126a33f0ec249f14edeaac8764b6
Rajat Agrawal <agrawal.rajat.89@gmail.com>
2014-04-06 23:39:11 -0400

    Refactor JShape to use ComplexShape

--- lib/tetris/shape/j_shape.rb
+++ lib/tetris/shape/j_shape.rb
@@ -1,50 +1,8 @@
-require_relative 'shape'
-require_relative 'block'
+require_relative 'complex_shape'
 module Tetris
-  class J_Shape < Shape
-    attr_accessor :block_1, :block_2
-    attr_accessor :block_3, :block_4
-
-    def initialize(window, config={})
-      super
-      initialize_blocks(config[:color])
-    end
-
-    def height
-      2
-    end
-
-    def width
-      3
-    end
-
-    def block_coordinates
-      blocks.map do |block|
-        [block.x, block.y, block.color]
-      end
-    end
-
-    def draw
-      blocks.each do |block|
-        block.draw
-      end
-    end
-
-    def move(direction)
-      super
-      blocks.each do |block|
-        block.move(direction)
-      end
-    end
+  class J_Shape < ComplexShape
 
     private
-
-    def blocks
-      [ block_1,
-        block_2,
-        block_3,
-        block_4]
-    end
 
     def initialize_blocks(color)
       @block_1 = Block.new(window, { x: x,
