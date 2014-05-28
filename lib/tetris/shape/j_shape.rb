db956c5ecd1fe5d145c856c9cdd74685570d45b0
Rajat Agrawal <agrawal.rajat.89@gmail.com>
2014-05-28 07:47:32 -0400

    Rotation coordinates for L shape

--- lib/tetris/shape/j_shape.rb
+++ lib/tetris/shape/j_shape.rb
@@ -2,6 +2,32 @@ require_relative 'complex_shape'
 module Tetris
   class J_Shape < ComplexShape
 
+    def rotated_coordinates(orientation)
+      case orientation
+      when '0_degrees'
+        [@x, @y - 1]
+      when '90_degrees'
+        [@x + 2, @y]
+      when '180_degrees'
+        [@x, @y + 1]
+      when '270_degrees'
+        [@x - 2, @y]
+      end
+    end
+
+    def rotated_block_coordinates(orientation)
+      case orientation
+      when '0_degrees'
+        [[@x, @y - 1],[@x, @y],[@x + 1, @y],[@x + 2, @y]]
+      when '90_degrees'
+        [[@x + 2, @y],[@x + 1, @y],[@x + 1, @y + 1],[@x + 1, @y + 2]]
+      when '180_degrees'
+        [[@x, @y + 1],[@x, @y],[@x - 1, @y],[@x - 2, @y]]
+      when '270_degrees'
+        [[@x - 2, @y],[ @x - 1, @y],[@x - 1, @y - 1],[@x - 1, @y - 2]]
+      end
+    end
+
     private
 
     def initialize_blocks(color)
