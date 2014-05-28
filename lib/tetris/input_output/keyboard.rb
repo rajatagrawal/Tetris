3d7fd0e1c150b9fd62a465711e457d070e6c7289
Rajat Agrawal <agrawal.rajat.89@gmail.com>
2014-05-28 09:08:39 -0400

    Improve keyboard responsivenes for interaction

--- lib/tetris/input_output/keyboard.rb
+++ lib/tetris/input_output/keyboard.rb
@@ -4,11 +4,25 @@ module Tetris
 
       def keyboard_listener
         if button_down? Gosu::KbLeft
+          if (@keyboard_press % @keyboard_press_interval) == 0
             @game_strategy.move_shape 'left'
+          end
+          @keyboard_press +=1
         elsif button_down? Gosu::KbRight
-          @game_strategy.move_shape 'right'
+          if (@keyboard_press % @keyboard_press_interval) == 0
+            @game_strategy.move_shape 'right'
+          end
+          @keyboard_press +=1
         elsif button_down? Gosu::KbDown
-          @game_strategy.move_shape 'down'
+          if (@keyboard_press % @keyboard_press_interval) == 0
+            @game_strategy.move_shape 'down'
+          end
+          @keyboard_press +=1
+        elsif button_down? Gosu::KbUp
+          if (@keyboard_press % (@keyboard_press_interval+5)) == 0
+            @game_strategy.rotate_shape
+          end
+          @keyboard_press +=1
         end
       end
     end
--- lib/tetris/main.rb
+++ lib/tetris/main.rb
@@ -7,24 +7,37 @@ module Tetris
     include InputOutput::Keyboard
 
     def initialize
-      super(1280,1080,false)
+      super(1280,1080,false, 1)
       self.caption=('Tetris')
-      config = { width: 6,
-                 height: 20,
+      config = { width: 10,
+                 height: 25,
                  unit_side: 40 }
       @game_strategy = GameStrategy::Main.new(self, config)
+      @counter = 0
+      @time_interval = 17
+      @keyboard_interval = 5
+      @keyboard_press_interval = 5
+      @keyboard_press = 0
     end
 
     def update
       keyboard_listener
-      sleep(0.1)
-      @game_strategy.run_game
+
+      if (@counter % @time_interval) == 0
+        @counter = 0
+        @game_strategy.run_game
+      end
+      @counter +=1
     end
 
     def draw
-      @game_strategy.shape.draw
-      @game_strategy.draw_map
-      @game_strategy.show_score
+        @game_strategy.shape.draw
+        @game_strategy.draw_map
+        @game_strategy.show_score
+    end
+
+    def button_up(id)
+      @keyboard_press = 0
     end
   end
 end
