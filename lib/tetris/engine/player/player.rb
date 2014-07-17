c42920f15a4f5806b7c1716affec64328f59800b
Rajat Agrawal <agrawal.rajat.89@gmail.com>
2014-07-17 07:41:02 -0400

    Refactor calculation of player number

--- lib/tetris/engine/player/player.rb
+++ lib/tetris/engine/player/player.rb
@@ -1,21 +1,26 @@
 module Tetris
   class Player
-    attr_accessor :shape, :next_shape, :score, :number
 
-    def self.next_number
-      @last_number ||= -1
-      @last_number += 1
-    end
+    attr_reader :score, :number
+    attr_accessor :shape, :next_shape
+
+    @@next_number = 0
 
     def initialize
       @score = 0
       @shape = nil
       @next_shape = nil
-      @number = self.class.next_number
+      @number = player_number
     end
 
     def increase_score(score)
       @score += score
+    end
+
+    private
+
+    def player_number
+      @@next_number += 1
     end
   end
 end
