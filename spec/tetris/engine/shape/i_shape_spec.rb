f01c5428e868b6666ee9f2a14e64fcf3b5c4c637
Rajat Agrawal <pair+rajat@pivotallabs.com>
2014-11-14 09:52:41 -0500

    Clean up tests for IShape

--- spec/tetris/engine/shape/i_shape_spec.rb
+++ spec/tetris/engine/shape/i_shape_spec.rb
@@ -1,55 +1,19 @@
-require 'spec_helper'
-require 'tetris/shape/i_shape'
+require 'tetris/engine/shape/i_shape'
+
 module Tetris
   describe I_Shape do
-    let(:window) { double 'window' }
+    let(:i_shape) { I_Shape.new }
 
-    describe '#initialize' do
-      it 'creates and configures top left block' do
-        config = { x: 10, y: 10, unit_side: 15, color: 'blue' }
-        shape = I_Shape.new(window, config)
-        expect(shape.block_1.x).to eq 10
-        expect(shape.block_1.y).to eq 10
-        expect(shape.block_1.color).to eq Gosu::Color::BLUE
-      end
-
-      it 'creates and configures top right block' do
-        config = { x: 10, y: 10, unit_side: 15, color: 'blue' }
-        shape = I_Shape.new(window, config)
-        expect(shape.block_2.x).to eq 11
-        expect(shape.block_2.y).to eq 10
-        expect(shape.block_2.color).to eq Gosu::Color::BLUE
-      end
-
-      it 'creates and configures bottom left block' do
-        config = { x: 10, y: 10, unit_side: 15, color: 'blue' }
-        shape = I_Shape.new(window, config)
-        expect(shape.block_3.x).to eq 12
-        expect(shape.block_3.y).to eq 10
-        expect(shape.block_3.color).to eq Gosu::Color::BLUE
-      end
-
-
-      it 'creates and configures bottom right block' do
-        config = { x: 10, y: 10, unit_side: 15, color: 'blue' }
-        shape = I_Shape.new(window, config)
-        expect(shape.block_4.x).to eq 13
-        expect(shape.block_4.y).to eq 10
-        expect(shape.block_4.color).to eq Gosu::Color::BLUE
-      end
-    end
-
-    describe '#height' do
-      it 'returns height of shape' do
-        shape = I_Shape.new window
-        expect(shape.height).to eq 1
-      end
-    end
-
-    describe '#width' do
-      it 'returns width of shape' do
-        shape = I_Shape.new window
-        expect(shape.width).to eq 4
+    describe '#coordinates_for' do
+      it 'returns correct coordinates for the forming blocks' do
+        expect(i_shape.coordinates_for('0_degrees')).
+          to eq([ [1,1], [2,1], [3,1], [4,1] ])
+        expect(i_shape.coordinates_for('90_degrees')).
+          to eq([ [2,0], [2,1], [2,2], [2,3] ])
+        expect(i_shape.coordinates_for('180_degrees')).
+          to eq([ [1,1], [2,1], [3,1], [4,1] ])
+        expect(i_shape.coordinates_for('270_degrees')).
+          to eq([ [2,0], [2,1], [2,2], [2,3] ])
       end
     end
   end
